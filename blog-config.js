13456c27f50864a15ec7177fb534631baa5748d5
hgo641 <hgo641@gmail.com>
2022-09-15 13:26:00 +0900

    chore: modify siteUrl

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "hongo",
   description: "홍고 블로그",
   author: "hongo",
-  siteUrl: "https://hongo.netlify.app/",
+  siteUrl: "https://blog.hongo.app",
   links: {
     github: "https://github.com/hgo641",
   },
